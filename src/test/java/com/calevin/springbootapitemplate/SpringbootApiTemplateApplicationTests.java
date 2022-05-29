6b997e2c39d4bf12d7cd44daa885d29640a942db
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2022-05-29 10:20:54 -0300

    Updated contextLoads test

--- src/test/java/com/calevin/springbootapitemplate/SpringbootApiTemplateApplicationTests.java
+++ src/test/java/com/calevin/springbootapitemplate/SpringbootApiTemplateApplicationTests.java
@@ -1,13 +1,35 @@
 package com.calevin.springbootapitemplate;
 
+import com.calevin.springbootapitemplate.controllers.EntityExampleController;
+import com.calevin.springbootapitemplate.repositories.CategoryExampleRepository;
+import com.calevin.springbootapitemplate.repositories.EntityExampleRepository;
+import com.calevin.springbootapitemplate.services.EntityExampleService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SpringbootApiTemplateApplicationTests {
 
+	@Autowired
+	private EntityExampleController entityExampleController;
+
+	@Autowired
+	private EntityExampleRepository entityExampleRepository;
+
+	@Autowired
+	private CategoryExampleRepository categoryExampleRepository;
+
+	@Autowired
+	private EntityExampleService entityExampleService;
 	@Test
 	void contextLoads() {
+		assertThat(entityExampleController).isNotNull();
+		assertThat(entityExampleRepository).isNotNull();
+		assertThat(categoryExampleRepository).isNotNull();
+		assertThat(entityExampleService).isNotNull();
 	}
 
 }
