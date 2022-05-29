e0dc78a12e02640cd511c00f10bd1d23bb671f7f
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2022-05-29 10:18:09 -0300

    Added Repository annotation

--- src/main/java/com/calevin/springbootapitemplate/repositories/EntityExampleRepository.java
+++ src/main/java/com/calevin/springbootapitemplate/repositories/EntityExampleRepository.java
@@ -2,6 +2,8 @@ package com.calevin.springbootapitemplate.repositories;
 
 import com.calevin.springbootapitemplate.entities.EntityExample;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EntityExampleRepository extends JpaRepository<EntityExample, Long> {
 }
