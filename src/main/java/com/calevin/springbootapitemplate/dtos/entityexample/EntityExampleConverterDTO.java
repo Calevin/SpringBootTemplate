a37de434ad83edd2e119c85fa4e2a506e60e8ca6
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2022-05-30 03:20:08 -0300

    now ConverterDTO not use repository

--- src/main/java/com/calevin/springbootapitemplate/dtos/entityexample/EntityExampleConverterDTO.java
+++ src/main/java/com/calevin/springbootapitemplate/dtos/entityexample/EntityExampleConverterDTO.java
@@ -1,28 +1,17 @@
 package com.calevin.springbootapitemplate.dtos.entityexample;
 
-import com.calevin.springbootapitemplate.entities.CategoryExample;
 import com.calevin.springbootapitemplate.entities.EntityExample;
-import com.calevin.springbootapitemplate.repositories.CategoryExampleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Slf4j
 @Mapper(componentModel = "spring")
 public abstract class EntityExampleConverterDTO {
 
-    @Autowired
-    CategoryExampleRepository categoryExampleRepository;
-
     @Mapping(target = "categoryName", source = "entityExample.category.name")
     public abstract GetEntityExampleDTO converterToGetEntityExampleDTO(EntityExample entityExample);
 
-    @Mapping(target = "category", expression = "java( getCategoryExample( createEntityExampleDTO.getCategoryId() ) )")
+    @Mapping(target = "category", expression = "java( CategoryExample.builder().id( createEntityExampleDTO.getCategoryId() ).build() )")
     public abstract EntityExample converterToEntityExample(CreateEntityExampleDTO createEntityExampleDTO);
-
-    protected CategoryExample getCategoryExample(Long categoryId) {
-        log.info("getCategoryExample, parameter categoryId: {}", categoryId);
-        return categoryExampleRepository.findById(categoryId).orElse(null);
-    }
 }
